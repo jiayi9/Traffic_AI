19f7386809713b708d4c1f6896e652ea175bc003
Jiayi Lu <215756347@qq.com>
2018-11-05 13:46:01 +0800

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  5 13:33:45 2018
+
+@author: LUI8WX
+"""
+
+from dateutil.parser import parse
+
+x = parse('2018-07-12')
+
+x2 = parse('2018-07-12 13:20:20.2')
+
+delta = x2 - x
+
+x.date()
+
+
+x1 = parse('2018-07-12')
+x2 = parse('2018-07-13')
+
+delta = x2 - x1
+
+delta.days
+
+print(d)
+d + datetime.timedelta(days = 1)
+d + datetime.timedelta(days = 2)
+
+d + datetime.timedelta(seconds = 360000)
+
+import datetime
+
+d = datetime.date.today()
+
+d.
+
+x1
+
+d - x1
+
+
+x2.date() - d
+
+
